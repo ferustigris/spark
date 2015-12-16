63e1144821da5960b5450e6fd7a5329e5c2dc4b8
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2015-12-16 17:52:49 +0300

    Task1 has been implemented (theoretically)

--- src/main/scala/task1.scala
+++ src/main/scala/task1.scala
@@ -3,8 +3,8 @@ import scala.util.matching.Regex
 import scala.util.matching.Regex.MatchIterator
 
 object task1 {
-  def getRecord(iter: MatchIterator): Tuple2[String, Int] = {
-    (iter.group(0), iter.group(7).toInt)
+  def getRecord(iter: MatchIterator): Tuple2[String, Tuple2[Int, Int] ] = {
+    (iter.group(0), (1, iter.group(7).toInt))
   }
   def main (args: Array[String]) {
     val conf = new SparkConf().setAppName("task1").setMaster("local")
@@ -12,28 +12,10 @@ object task1 {
 
     // put some data in an RDD
     val f = sc.textFile("000000")
-    val hz = f.map(line => getRecord("([^\"]\\S*|\".+?\")\\s*".r.findAllIn(line)))
+    val records = f.map(line => getRecord("([^\"]\\S*|\".+?\")\\s*".r.findAllIn(line)))
+    var res = records.reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2))
 
-    val numbersRDD = sc.parallelize(numbers, 4)
-    println("Print each element of the original RDD")
-    numbersRDD.foreach(println)
-
-    // trivially operate on the numbers
-    val stillAnRDD = numbersRDD.map(n => n.toDouble / 10)
-
-    // get the data back out
-    val nowAnArray = stillAnRDD.collect()
-    // interesting how the array comes out sorted but the RDD didn't
-    println("Now print each element of the transformed array")
-    nowAnArray.foreach(println)
-
-    // explore RDD properties
-    val partitions = stillAnRDD.glom()
-    println("We _should_ have 4 partitions")
-    println(partitions.count())
-    partitions.foreach(a => {
-      println("Partition contents:" +
-        a.foldLeft("")((s, e) => s + " " + e))
-    })
+    println("Result:")
+    res.foreach(x => print(x._1 + ", " + x._2._1 + ", " + x._2._2))
   }
 }
