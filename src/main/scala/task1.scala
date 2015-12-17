5658c69a10c9572cf83253e38c55e5fe0b210860
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2015-12-17 21:49:35 +0300

    Task1 has been implemented

--- src/main/scala/task1.scala
+++ src/main/scala/task1.scala
@@ -1,21 +1,26 @@
-import org.apache.spark.{SparkContext, SparkConf}
-import scala.util.matching.Regex
+import org.apache.spark.{SparkConf, SparkContext}
+
 import scala.util.matching.Regex.MatchIterator
 
 object task1 {
   def getRecord(iter: MatchIterator): Tuple2[String, Tuple2[Int, Int] ] = {
-    (iter.group(0), (1, iter.group(7).toInt))
+    val lst =iter.toList
+    if (lst.length < 8) {
+      return ("", (0, 0))
+    }
+    (lst(0), (1, lst(7).trim.toInt))
   }
   def main (args: Array[String]) {
     val conf = new SparkConf().setAppName("task1").setMaster("local")
     val sc = new SparkContext(conf);
 
     // put some data in an RDD
-    val f = sc.textFile("000000")
-    val records = f.map(line => getRecord("([^\"]\\S*|\".+?\")\\s*".r.findAllIn(line)))
-    var res = records.reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2))
+    val f = sc.textFile("000001")
+    val records = f.map(line => { println(line); getRecord("([^\"]\\S*|\".+?\")\\s*".r.findAllIn(line)) })
+    println(records)
+    var res = records.reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2)).map(a => (a._1, (a._2._2/a._2._1, a._2._2)))
 
     println("Result:")
-    res.foreach(x => print(x._1 + ", " + x._2._1 + ", " + x._2._2))
+    res.foreach(x => println(x._1 + ", " + x._2._1 + ", " + x._2._2))
   }
 }
