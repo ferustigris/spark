aaab30676c76627b8cd2022ae276d0f058452bdd
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2015-12-27 23:22:27 +0300

    States have been added

--- src/main/scala/CustomReceiver.scala
+++ src/main/scala/CustomReceiver.scala
@@ -7,11 +7,7 @@ import org.jnetpcap.packet.{PcapPacket, PcapPacketHandler}
 class CustomReceiver extends Receiver[Integer](StorageLevel.MEMORY_ONLY) with Logging {
   class PackHandler extends PcapPacketHandler[String] {
     override def nextPacket(pcapPacket: PcapPacket, t: String): Unit = {
-      Thread sleep(500)
-      println("==>")
-      println(pcapPacket.getCaptureHeader.caplen())
-      println("=>")
-
+      Thread sleep(300)
       store(pcapPacket.getCaptureHeader.caplen())
     }
   }
@@ -27,14 +23,10 @@ class CustomReceiver extends Receiver[Integer](StorageLevel.MEMORY_ONLY) with Lo
   }
 
   private def receive() {
-    var errbuf = new java.lang.StringBuilder();
+    var errbuf = new java.lang.StringBuilder()
     var pcap = Pcap.openOffline("pcap.pcap", errbuf)
     var handler = new PackHandler
-    //pcap.loop(10, handler, "")
-    while (true) {
-      store(100)
-      Thread sleep(500)
-    }
+    pcap.loop(10, handler, "")
   }
 
 }
--- src/main/scala/task3.scala
+++ src/main/scala/task3.scala
@@ -2,20 +2,46 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 
 object task3 {
+  val Threshold = 1000
+  val Limit = 1000
 
+  abstract class NetGlobalState
+  case class LimitNorm() extends NetGlobalState
+  case class LimitExceeded() extends NetGlobalState
+  var curGlobalState: NetGlobalState = new LimitNorm
+
+  abstract class NetLocalState
+  case class ThresholdNorm() extends NetLocalState
+  case class ThresholdExceeded() extends NetLocalState
+  var curState: NetLocalState = new ThresholdNorm
+
+
+  def updateFunction(newValues: Seq[Integer], runningCount: Option[Integer]): Option[Integer] = {
+    val count = runningCount.getOrElse[Integer](0) + newValues.reduce((a,b)=> a + b)
+    val prevState = curGlobalState
+    curGlobalState = if (count < Limit) new LimitNorm else new LimitExceeded
+
+    if (!curGlobalState.equals(prevState)) {
+      println("Global state has been changed to " + curGlobalState)
+    }
+    Some(count)
+  }
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("task3").setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(1))
     ssc.checkpoint(".")
 
     val data = ssc.receiverStream(new CustomReceiver())
-    data.reduceByWindow((a,b) => a + b, Seconds(1), Seconds(1)).map(a => {
-      a match {
-        case n: Integer if n < 100 => println("ThresholdNorm " + n)
-        case n: Integer if n >= 100 => println("ThresholdExceeded " + n)
+    var d = data.reduceByWindow((a,b) => a + b, Seconds(1), Seconds(1)).map(a => {
+      val prevState = curState
+      curState = if (a < Threshold) new ThresholdNorm else new ThresholdExceeded
+
+      if (!curState.equals(prevState)) {
+        println("Local state has been changed to " + curState)
       }
+      (1, a)
     }
-    )
+    ).updateStateByKey(updateFunction _)
     .print()
 
     ssc.start()
