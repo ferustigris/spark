bd8d6966e1ab1a3f9617930fb53e8c5050a30a2f
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2015-12-27 22:18:59 +0300

    ThresholdNorm&ThresholdExceeded have been added

--- src/main/scala/CustomReceiver.scala
+++ src/main/scala/CustomReceiver.scala
@@ -30,7 +30,11 @@ class CustomReceiver extends Receiver[Integer](StorageLevel.MEMORY_ONLY) with Lo
     var errbuf = new java.lang.StringBuilder();
     var pcap = Pcap.openOffline("pcap.pcap", errbuf)
     var handler = new PackHandler
-    pcap.loop(10, handler, "")
+    //pcap.loop(10, handler, "")
+    while (true) {
+      store(100)
+      Thread sleep(500)
+    }
   }
 
 }
--- src/main/scala/task3.scala
+++ src/main/scala/task3.scala
@@ -1,14 +1,21 @@
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
 
 object task3 {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("task3").setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(1))
+    ssc.checkpoint(".")
 
     val data = ssc.receiverStream(new CustomReceiver())
-    data.reduceByWindow((a,b) => a + b, Seconds(1), Seconds(1))
+    data.reduceByWindow((a,b) => a + b, Seconds(1), Seconds(1)).map(a => {
+      a match {
+        case n: Integer if n < 100 => println("ThresholdNorm " + n)
+        case n: Integer if n >= 100 => println("ThresholdExceeded " + n)
+      }
+    }
+    )
     .print()
 
     ssc.start()
